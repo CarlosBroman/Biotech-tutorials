757441656898eefb1783cd0b08ed538e88ec2a0a
Carlos Román <carlos.roman.baena@gmail.com>
2023-08-07 17:09:09 +0200

    Finding active molecules for our target

--- AI/Drug-discovery/first.py
+++ AI/Drug-discovery/first.py
@@ -15,3 +15,32 @@ activity = new_client.activity
 res = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50")
 df = pd.DataFrame.from_dict(res)
 print(df.head(3))
+
+df.standard_type.unique()
+
+df.to_csv('bioactivity_data.csv', index = False)
+
+df2 = df[df.standard_value.notna()]
+print(df2)
+
+bioactivity_class = []
+for i in df2.standard_value:
+    if float(i) >= 10000:
+        bioactivity_class.append("inactive")
+    elif float(i) <= 1000:
+        bioactivity_class.append("active")
+    else:
+        bioactivity_class.append("intermediate")
+        
+
+mol_cid = []
+for i in df2.molecule_chembl_id:
+    mol_cid.append(i)
+    
+canonical_smiles = []
+for i in df2.canonical_smiles:
+    canonical_smiles.append(i)
+    
+standard_value = []
+for i in df2.standard_value:
+    standard_value.append(i)
